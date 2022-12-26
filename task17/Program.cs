5e129e8b62f0269516dfec9d54f30e85baac3ed9
levin_serg <levineserg@gmail.com>
2022-12-27 00:06:04 +0400

    Семинар 3

---
+++ task17/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Веедите координату x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Веедите координату y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x>0 && y>0)
+{
+    Console.WriteLine("Точка находится в первой четверти");
+}
+else if(x<0 && y>0)
+{
+    Console.WriteLine("Точка находится во второй четверти");
+}
+else if(x<0 && y<0)
+{
+    Console.WriteLine("Точка находится в третьей четверти");
+}
+else 
+{
+    Console.WriteLine("Точка находится в четвертой четверти");
+}
---
+++ task18/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.Write("Веедите номер четверти: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a == 1)
+{
+    Console.WriteLine("Х>0;y>0");
+}
+else if(a==2)
+{
+    Console.WriteLine("Х<0;y>0");
+}
+else if(a==3)
+{
+    Console.WriteLine("Х<0;y<0");
+}
+else if(a==4)
+{
+    Console.WriteLine("Х>0;y<0");
+}
+else
+{
+    Console.WriteLine("Такой четверти нет");
+}

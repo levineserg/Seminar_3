c8c66a8147fc1afeb9ffd44fbc9ff3cbeea101bf
levin_serg <levineserg@gmail.com>
2022-12-27 00:45:55 +0400

    task18

--- task17/Program.cs
+++ task17/Program.cs
@@ -3,19 +3,19 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.Write("Веедите координату y: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
-if(x>0 && y>0)
+if (x > 0 && y > 0)
 {
     Console.WriteLine("Точка находится в первой четверти");
 }
-else if(x<0 && y>0)
+else if (x < 0 && y > 0)
 {
     Console.WriteLine("Точка находится во второй четверти");
 }
-else if(x<0 && y<0)
+else if (x < 0 && y < 0)
 {
     Console.WriteLine("Точка находится в третьей четверти");
 }
-else 
+else
 {
     Console.WriteLine("Точка находится в четвертой четверти");
 }
--- task18/Program.cs
+++ task18/Program.cs
@@ -1,23 +1,37 @@
 ﻿// Напишите программу, которая по заданному номеру четверти, 
 // показывает диапазон возможных координат точек в этой четверти (x и y).
-Console.Write("Веедите номер четверти: ");
-int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Веедите номер четверти: ");
+// int a = Convert.ToInt32(Console.ReadLine());
 
-if(a == 1)
+// if (a == 1)
+// {
+//     Console.WriteLine("Х>0;y>0");
+// }
+// else if (a == 2)
+// {
+//     Console.WriteLine("Х<0;y>0");
+// }
+// else if (a == 3)
+// {
+//     Console.WriteLine("Х<0;y<0");
+// }
+// else if (a == 4)
+// {
+//     Console.WriteLine("Х>0;y<0");
+// }
+// else
+// {
+//     Console.WriteLine("Такой четверти нет");
+// }
+string[] quaters = {"Х>0;y>0",
+                    "Х<0;y>0",
+                    "Х<0;y<0",
+                    "Х>0;y<0"};
+Console.Write("Веедите номер четверти: ");
+int quater = Convert.ToInt32(Console.ReadLine());
+if (quater >= 1 && quater <= 4)
 {
-    Console.WriteLine("Х>0;y>0");
-}
-else if(a==2)
-{
-    Console.WriteLine("Х<0;y>0");
-}
-else if(a==3)
-{
-    Console.WriteLine("Х<0;y<0");
-}
-else if(a==4)
-{
-    Console.WriteLine("Х>0;y<0");
+    Console.WriteLine(quaters[quater - 1]);
 }
 else
 {

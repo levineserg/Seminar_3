bbbe6d7f1ce4595efb14fe5009eb319adf075499
levin_serg <levineserg@gmail.com>
2022-12-29 16:44:42 +0400

    добавил задачи

---
+++ task20/Program.cs
@@ -0,0 +1,19 @@
+﻿// Math.Sqrt() - нахождение квадратного корня
+// Math.Pow(переменная, степень) - возведение в степень
+// Math.Round(переменная, количество знаков до которого округлить) - округление числа
+// double - числа с запятой
+// 
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+Console.Write("Веедите координату x1: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Веедите координату y1: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Веедите координату x2: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Веедите координату y2: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+double distans = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+Console.WriteLine("Расстояние между точками " + Math.Round(distans, 2));
---
+++ task22/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// Console.Write("Веедите число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// while(count <= number)
+// {
+//     Console.Write(Math.Pow(count, 2)+" ");
+//     count++;
+// }
+Console.Write("Веедите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+for(int count = 1; count <= number; count++)
+{
+    Console.Write(Math.Pow(count, 2)+" ");
+}
